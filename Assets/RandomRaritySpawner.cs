ec1ef56b768e09e719f7e3ea19fd0b59c14a9ed8
Manuele Giorgiutti <manuelegiorgiutti@gmail.com>
2025-04-05 15:36:52 +0200

    Assets

--- Assets/RandomRaritySpawner.cs
+++ Assets/RandomRaritySpawner.cs
@@ -1,39 +1,48 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
     [Serializable]
-    public class SpawnObject {
+    public class SpawnObject
+    {
         public GameObject spawnObject;
         public float probability;
     }
 
     [SerializeField] private List<SpawnObject> spawnObjects = new List<SpawnObject>();
-    [SerializeField] float spawnTime;
+    [SerializeField] private float spawnTime = 3.0f; // Tempo tra un spawn e l'altro
 
     private float lastSpawnTime;
     private float currentSpawnTimer;
-    private float[] probabilities; 
+    private float[] probabilities;  
     private float IncSpawn=0;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Calcola le probabilità cumulative normalizzate
         probabilities = new float[spawnObjects.Count];
+        float totalProbability = 0.0f;
 
-        float current_probability = 0.0f;
-        for(int index=0; index<spawnObjects.Count; ++index)
+        // Calcola la somma totale delle probabilità
+        foreach (var spawnObj in spawnObjects)
         {
-            current_probability += spawnObjects[index].probability;
-            probabilities[index] = current_probability;
+            totalProbability += spawnObj.probability;
+        }
+
+        // Normalizza le probabilità per farle sommare a 1
+        float current_probability = 0.0f;
+        for (int i = 0; i < spawnObjects.Count; ++i)
+        {
+            current_probability += spawnObjects[i].probability / totalProbability;  // Normalizza
+            probabilities[i] = current_probability;
         }
 
         lastSpawnTime = 0;
-        currentSpawnTimer = spawnTime + IncSpawn;
+        currentSpawnTimer = spawnTime;
     }
 
     // Update is called once per frame
@@ -41,31 +50,54 @@ public class Spawner : MonoBehaviour
     {
         lastSpawnTime += Time.deltaTime;
 
-        if(lastSpawnTime > currentSpawnTimer){
-
-            float spawnX = UnityEngine.Random.Range(-20.0f,-10.0f);
+        if (lastSpawnTime > currentSpawnTimer)
+        {
+            // Posizione di spawn casuale
+            float spawnX;
+            if (UnityEngine.Random.value < 0.5f) // 50% di probabilità per ogni intervallo
+            {
+                spawnX = UnityEngine.Random.Range(-20.0f, -15.0f); // Intervallo negativo
+            }
+            else
+            {
+                spawnX = UnityEngine.Random.Range(-10.0f, -5.0f); // Intervallo positivo
+            }
             float spawnY = 0.98f;
-            float spawnZ = UnityEngine.Random.Range(-20.0f,20.0f);
-            Vector3 spawnPoint = new Vector3(spawnX,spawnY,spawnZ);
+            float spawnZ;
+            if (UnityEngine.Random.value < 0.5f) // 50% di probabilità per ogni intervallo
+            {
+                spawnZ = UnityEngine.Random.Range(10.0f, 15.0f); // Intervallo negativo
+            }
+            else
+            {
+                spawnZ = UnityEngine.Random.Range(20.0f, 25.0f); // Intervallo positivo
+            }
+            Vector3 spawnPoint = new Vector3(spawnX, spawnY, spawnZ);
 
-            // Determine which object to spawn
-            float spawnRandom = UnityEngine.Random.Range(0.0f,2.0f);
+            // Determina quale oggetto spawnare in base alla probabilità
+            float spawnRandom = UnityEngine.Random.Range(0.0f, 1.0f); // 0.0f a 1.0f, non più 2.0f
+
             int index = 0;
-            while(index<probabilities.Length && spawnRandom > probabilities[index]){
+            while (index < probabilities.Length && spawnRandom > probabilities[index])
+            {
                 ++index;
             }
 
-            if(index < probabilities.Length){
+            if (index < probabilities.Length)
+            {
+                // Istanzia l'oggetto selezionato
                 GameObject newObject = Instantiate(spawnObjects[index].spawnObject);
                 newObject.transform.position = spawnPoint;
-            } else {
+            }
+            else
+            {
                 Debug.Log("Spawner: Probability out of range");
             }
 
-            IncSpawn ++;
+            // Reset del timer di spawn
+            IncSpawn++;
             lastSpawnTime = 0;
-            currentSpawnTimer = spawnTime + IncSpawn;
+            currentSpawnTimer = spawnTime+ IncSpawn;
         }
-        
     }
 }
