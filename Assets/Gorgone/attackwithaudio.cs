317e9438ba89877a713752750c272250fc64c8cb
Francesco Gorgone <francesco.gorgone14@gmail.com>
2025-04-06 15:12:04 +0200

    sound_updates

--- Assets/Gorgone/attackwithaudio.cs
+++ Assets/Gorgone/attackwithaudio.cs
@@ -5,19 +5,31 @@ public class xcv : MonoBehaviour
 
     private Animator mAnimator;
     private bool isSprinting;
+    public AudioSource walk_sound;
+    private bool walking = false;
     public bool isHappy;
 
     void Start()
     {
         mAnimator = GetComponent<Animator>();
             mAnimator.SetBool("Victory", isHappy);
+                walk_sound = GetComponent<AudioSource>();
+    
         
     }
 
 
     public void OnMove(InputAction.CallbackContext context) {
-        isSprinting = context.started || context.performed;
+        walking = true;
+        if(walking) {
+    isSprinting = context.started || context.performed;
+        walk_sound.Play();
         mAnimator.SetBool("Running", isSprinting);
+        }
+        else {
+            walking = false;
+            walk_sound.Stop();
+        }
          }
 
     public void OnAttack(InputAction.CallbackContext context) {
