a2faeef377061e688c0e9ad0f51bb21ae89f650a
termosifone06 <88204634+termosifone06@users.noreply.github.com>
2025-04-06 21:42:18 +0200

    AddedPointCollectionSystem
    Found an important bug: restarting / trying again the level doeesn't properly reset it

--- Assets/prova_trigger.cs
+++ Assets/prova_trigger.cs
@@ -1,13 +1,26 @@
 using UnityEngine;
 
 public class TriggerScript : MonoBehaviour
-{
+{   
+    private EventManager eventManager; // Riferimento all'EventManager
+    public int oreValue = 1;
+
+    private  void Start(){
+        eventManager = FindObjectOfType<EventManager>(); //trova l'EventManager
+    }
+
     // Questa funzione viene chiamata quando il giocatore entra in contatto con il trigger
     public void AttivaFunzione()
     {
         // Logica da eseguire quando il trigger viene attivato
         Debug.Log("Il trigger è stato attivato dal giocatore!");
         // Aggiungi altre azioni qui (ad esempio, distruggere l'oggetto, fare qualcosa nella scena, ecc.)
+
+
+        // Aggiungi punti al punteggio del giocatore
+        eventManager.AddPoints(oreValue);
+
+
         Destroy(gameObject);
     }
 }
