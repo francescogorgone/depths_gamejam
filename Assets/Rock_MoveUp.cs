6efdb47b5e814e2c04b0cd5110af1c3c07216e45
termosifone06 <88204634+termosifone06@users.noreply.github.com>
2025-04-07 00:10:39 +0200

    Update Rock_MoveUp.cs

--- Assets/Rock_MoveUp.cs
+++ Assets/Rock_MoveUp.cs
@@ -2,13 +2,23 @@ using UnityEngine;
 
 public class MoveAndDestroy : MonoBehaviour
 {
-    public float speed = 5f;  // Velocità di salita lungo l'asse Y
+
+    private EventManager eventManager; //accellerazione
+
+    private float baseSpeed = 3f;  // Velocità di salita lungo l'asse Y
+     private float Speed = 0f;
     public float targetY = 100f;  // Altezza target raggiunta la quale l'oggetto viene distrutto
+
+    void Start(){
+        eventManager = FindFirstObjectByType<EventManager>();
+    }
 
     void Update()
     {
+        Speed = baseSpeed + 20 * eventManager.speed;
+
         // Muovi l'oggetto lungo l'asse Y
-        transform.Translate(Vector3.up * speed * Time.deltaTime);
+        transform.Translate(Vector3.up * Speed * Time.deltaTime);
 
         // Controlla se l'oggetto ha raggiunto o superato il targetY
         if (transform.position.y >= targetY)
