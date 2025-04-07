6ad7c1ae5bc65d89ff64f0f18e79e2b168f3b4bb
termosifone06 <88204634+termosifone06@users.noreply.github.com>
2025-04-07 16:05:07 +0200

    test

--- Assets/GemBox.cs
+++ Assets/GemBox.cs
@@ -3,9 +3,12 @@ using UnityEngine;
 public class GemBox : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private EventManager eventManager;
+
     void Start()
     {
-        
+        eventManager = FindFirstObjectByType<EventManager>();
     }
 
     // Update is called once per frame
