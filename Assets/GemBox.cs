f170d42747f781fba5cdae16710d5cd239dbba30
termosifone06 <88204634+termosifone06@users.noreply.github.com>
2025-04-07 16:32:34 +0200

    WorkingonABOX

--- Assets/GemBox.cs
+++ Assets/GemBox.cs
@@ -11,11 +11,19 @@ public class GemBox : MonoBehaviour
     void Start()
     {
         eventManager = FindFirstObjectByType<EventManager>();
+        GetComponent<MeshRenderer>().enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (eventManager.points >= scoreNeeded)
+        {
+          GetComponent<MeshRenderer>().enabled = false;
+        }
+        else
+        {
+          GetComponent<MeshRenderer>().enabled = true;
+        }
     }
 }
